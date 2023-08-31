f6be1ec90be173c2f44eda5ad43184add289949d
Xijing Zhang <zhangxijing97@gmail.com>
2023-08-31 13:24:46 -0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -13,6 +13,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import copy # a method that is used on objects to create copies of them
+import math
 
 # load data
 def load_data():
@@ -51,10 +53,8 @@ plt.title("Profits vs. Population per city")
 plt.ylabel('Profit in $10,000')
 # Set the x-axis label
 plt.xlabel('Population of City in 10,000s')
-# show the plot
-# plt.show()
 
-
+# 1. compute_cost
 def compute_cost(x, y, w, b):
     """
     Computes the cost function for linear regression.
@@ -74,6 +74,20 @@ def compute_cost(x, y, w, b):
     total_cost = 1 / (2 * m) * cost
     return total_cost
 
+def compute_cost2(x, y, w, b):
+    """
+    Computes the cost function for linear regression.
+        x (ndarray): Shape (m,) Input to the model (Population of cities)
+        y (ndarray): Shape (m,) Label (Actual profits for the cities)
+        w, b (scalar): Parameters of the model
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+    """
+    # cost function: 𝐽(𝑤,𝑏)=1/2𝑚∑(𝑓𝑤,𝑏(𝑥(𝑖))−𝑦(𝑖))^2
+
+    return total_cost
+
+
 # Compute cost with some initial values for paramaters w, b
 initial_w = 2
 initial_b = 1
@@ -81,3 +95,115 @@ cost = compute_cost(x_train, y_train, initial_w, initial_b)
 print(type(cost))
 print(f'Cost at initial w: {cost:.3f}')
 # Expected Output: Cost at initial w: 75.203
+
+# 2. compute_gradient
+# ∂𝐽(𝑤,𝑏)/∂𝑏 is dj_db
+# gradient = derivative = rate of change of a function
+# When w,b is a certain value, find the derivative of the cost function
+def compute_gradient(x, y, w, b):
+    """
+      x (ndarray): Shape (m,) Input to the model (Population of cities)
+      y (ndarray): Shape (m,) Label (Actual profits for the cities)
+      w, b (scalar): Parameters of the model
+    Returns
+      dj_dw (scalar): The gradient of the cost with respect to the parameters w
+      dj_db (scalar): The gradient of the cost with respect to the parameter b
+     """
+    # gradient descent:
+    # 𝑏=𝑏−𝛼 ∂𝐽(𝑤,𝑏)/∂𝑏
+    # ∂𝐽(𝑤,𝑏)/∂𝑏=1𝑚∑(𝑓𝑤,𝑏(𝑥(𝑖))−𝑦(𝑖))
+    # 𝑤=𝑤−𝛼 ∂𝐽(𝑤,𝑏)/∂𝑤
+    # ∂𝐽(𝑤,𝑏)/∂𝑤=1𝑚∑(𝑓𝑤,𝑏(𝑥(𝑖))−𝑦(𝑖))𝑥(𝑖)
+
+    # Number of training examples
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = f_wb - y[i]
+        dj_db += dj_db_i  # update derivative of ∂𝐽(𝑤,𝑏)/∂b
+        dj_dw += dj_dw_i  # update derivative of ∂𝐽(𝑤,𝑏)/∂w
+    dj_db = dj_db / m
+    dj_dw = dj_dw / m
+    return dj_dw, dj_db
+
+# Compute and display gradient with w initialized to zeroes
+initial_w = 0
+initial_b = 0
+tmp_dj_dw, tmp_dj_db = compute_gradient(x_train, y_train, initial_w, initial_b)
+print('Gradient at initial w, b (zeros):', tmp_dj_dw, tmp_dj_db)
+# Expected Output: Gradient at test w -47.41610118 -4.007175051546391
+
+# 3. gradient_descent
+def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
+    """
+      x :    (ndarray): Shape (m,)
+      y :    (ndarray): Shape (m,)
+      w_in, b_in : (scalar) Initial values of parameters of the model
+      cost_function: function to compute cost
+      gradient_function: function to compute the gradient
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (ndarray): Shape (1,) Updated values of parameters of the model after running gradient descent
+      b : (scalar)                Updated value of parameter of the model after running gradient descent
+    """
+    # number of training examples
+    m = len(x)
+    # An array to store cost J and w's at each iteration — primarily for graphing later
+    J_history = []
+    w_history = []
+    w = copy.deepcopy(w_in)  # avoid modifying global w within function
+    b = b_in
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters
+        dj_dw, dj_db = gradient_function(x, y, w, b)
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+        # Save cost J at each iteration
+        if i < 100000:  # prevent resource exhaustion
+            cost = cost_function(x, y, w, b)
+            J_history.append(cost)
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i % math.ceil(num_iters / 10) == 0:
+            w_history.append(w)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+    return w, b, J_history, w_history  # return w and J,w history for graphing
+
+# initialize fitting parameters. Recall that the shape of w is (n,)
+initial_w = 0.
+initial_b = 0.
+# some gradient descent settings
+iterations = 10000
+alpha = 0.01
+w,b,J_history,w_history = gradient_descent(x_train ,y_train, initial_w, initial_b, compute_cost, compute_gradient, alpha, iterations)
+print("w,b found by gradient descent:", w, b)
+
+m = x_train.shape[0]
+predicted = np.zeros(m)
+for i in range(m):
+    predicted[i] = w * x_train[i] + b
+
+# Plot the linear fit
+plt.plot(x_train, predicted, c = "b")
+# Create a scatter plot of the data.
+plt.scatter(x_train, y_train, marker='x', c='r')
+# Set the title
+plt.title("Profits vs. Population per city")
+# Set the y-axis label
+plt.ylabel('Profit in $10,000')
+# Set the x-axis label
+plt.xlabel('Population of City in 10,000s')
+
+# population test
+predict1 = 3.5 * w + b
+print('For population = 35,000, we predict a profit of $%.2f' % (predict1*10000))
+predict2 = 7.0 * w + b
+print('For population = 70,000, we predict a profit of $%.2f' % (predict2*10000))
+
+# show the plot
+plt.show()
