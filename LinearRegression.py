32eaf8ab5552d4446a3b79c1f63a1cca5d36f75d
Xijing Zhang <zhangxijing97@gmail.com>
2023-09-01 11:45:26 -0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -74,20 +74,6 @@ def compute_cost(x, y, w, b):
     total_cost = 1 / (2 * m) * cost
     return total_cost
 
-def compute_cost2(x, y, w, b):
-    """
-    Computes the cost function for linear regression.
-        x (ndarray): Shape (m,) Input to the model (Population of cities)
-        y (ndarray): Shape (m,) Label (Actual profits for the cities)
-        w, b (scalar): Parameters of the model
-    Returns
-        total_cost (float): The cost of using w,b as the parameters for linear regression
-    """
-    # cost function: 𝐽(𝑤,𝑏)=1/2𝑚∑(𝑓𝑤,𝑏(𝑥(𝑖))−𝑦(𝑖))^2
-
-    return total_cost
-
-
 # Compute cost with some initial values for paramaters w, b
 initial_w = 2
 initial_b = 1
@@ -119,7 +105,6 @@ def compute_gradient(x, y, w, b):
     m = x.shape[0]
     dj_dw = 0
     dj_db = 0
-
     for i in range(m):
         f_wb = w * x[i] + b
         dj_dw_i = (f_wb - y[i]) * x[i]
@@ -130,12 +115,46 @@ def compute_gradient(x, y, w, b):
     dj_dw = dj_dw / m
     return dj_dw, dj_db
 
+def compute_gradient2(x, y, w, b):
+    """
+          x (ndarray): Shape (m,) Input to the model (Population of cities)
+          y (ndarray): Shape (m,) Label (Actual profits for the cities)
+          w, b (scalar): Parameters of the model
+        Returns
+          dj_dw (scalar): The gradient of the cost with respect to the parameters w
+          dj_db (scalar): The gradient of the cost with respect to the parameter b
+         """
+    # gradient descent:
+    # 𝑏=𝑏−𝛼 ∂𝐽(𝑤,𝑏)/∂𝑏
+    # ∂𝐽(𝑤,𝑏)/∂𝑏=1𝑚∑(𝑓𝑤,𝑏(𝑥(𝑖))−𝑦(𝑖))
+    # 𝑤=𝑤−𝛼 ∂𝐽(𝑤,𝑏)/∂𝑤
+    # ∂𝐽(𝑤,𝑏)/∂𝑤=1𝑚∑(𝑓𝑤,𝑏(𝑥(𝑖))−𝑦(𝑖))𝑥(𝑖)
+
+    # Number of training examples
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+    for i in range(m):
+        f_wb = w*x[i]+b
+        dj_dw = dj_dw + (f_wb-y[i])*x[i]
+        dj_db = dj_db + (f_wb - y[i])
+
+    dj_dw = dj_dw/m
+    dj_db = dj_db / m
+    return dj_dw, dj_db
+
 # Compute and display gradient with w initialized to zeroes
 initial_w = 0
 initial_b = 0
-tmp_dj_dw, tmp_dj_db = compute_gradient(x_train, y_train, initial_w, initial_b)
+tmp_dj_dw, tmp_dj_db = compute_gradient2(x_train, y_train, initial_w, initial_b)
 print('Gradient at initial w, b (zeros):', tmp_dj_dw, tmp_dj_db)
-# Expected Output: Gradient at test w -47.41610118 -4.007175051546391
+# Gradient at initial w, b (zeros): -65.32884974555672 -5.83913505154639
+
+test_w = 0.2
+test_b = 0.2
+tmp_dj_dw, tmp_dj_db = compute_gradient2(x_train, y_train, test_w, test_b)
+print('Gradient at test w, b:', tmp_dj_dw, tmp_dj_db)
+# Gradient at test w, b: -47.41610118114435 -4.007175051546391
 
 # 3. gradient_descent
 def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
@@ -206,4 +225,4 @@ predict2 = 7.0 * w + b
 print('For population = 70,000, we predict a profit of $%.2f' % (predict2*10000))
 
 # show the plot
-plt.show()
+# plt.show()
