0f05de47e98c26f70984438fe34241e7db41bcba
Xijing Zhang <zhangxijing97@gmail.com>
2023-09-03 16:45:27 -0700

    Update

--- FeatureEngineeringandPolynomialRegression.py
+++ FeatureEngineeringandPolynomialRegression.py
@@ -9,7 +9,100 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from MultipleLinearRegression import gradient_descent
+
+# norm_plot
+def norm_plot(ax, data):
+    scale = (np.max(data) - np.min(data)) * 0.2
+    x = np.linspace(np.min(data) - scale, np.max(data) + scale, 50)
+    _, bins, _ = ax.hist(data, x, color="xkcd:azure")
+    # ax.set_ylabel("Count")
+
+    mu = np.mean(data);
+    std = np.std(data);
+    dist = norm.pdf(bins, loc=mu, scale=std)
+
+    axr = ax.twinx()
+    axr.plot(bins, dist, color="orangered", lw=2)
+    axr.set_ylim(bottom=0)
+    axr.axis('off')
+
+# plt_equal_scale
+def plt_equal_scale(X_train, X_norm, y_train):
+    fig,ax = plt.subplots(1,2,figsize=(12,5))
+    prange = [
+              [ 0.238-0.045, 0.238+0.045,  50],
+              [-25.77326319-0.045, -25.77326319+0.045, 50],
+              [-50000, 0,      50],
+              [-1500,  0,      50],
+              [0, 200000, 50]]
+    w_best = np.array([0.23844318, -25.77326319, -58.11084634,  -1.57727192])
+    b_best = 235
+    plt_contour_multi(X_train, y_train, w_best, b_best, ax[0], prange, 0, 1,
+                      title='Unnormalized, J(w,b), vs w[0],w[1]',
+                      xlabel= "w[0] (size(sqft))", ylabel="w[1] (# bedrooms)")
+    #
+    w_best = np.array([111.1972, -16.75480051, -28.51530411, -37.17305735])
+    b_best = 376.949151515151
+    prange = [[ 111-50, 111+50,   75],
+              [-16.75-50,-16.75+50, 75],
+              [-28.5-8, -28.5+8,  50],
+              [-37.1-16,-37.1+16, 50],
+              [376-150, 376+150, 50]]
+    plt_contour_multi(X_norm, y_train, w_best, b_best, ax[1], prange, 0, 1,
+                      title='Normalized, J(w,b), vs w[0],w[1]',
+                      xlabel= "w[0] (normalized size(sqft))", ylabel="w[1] (normalized # bedrooms)")
+    fig.suptitle("Cost contour with equal scale", fontsize=18)
+    #plt.tight_layout(rect=(0,0,1.05,1.05))
+    fig.tight_layout(rect=(0,0,1,0.95))
+    plt.show()
+
+# plot_cost_i_w
+def plot_cost_i_w(X,y,hist):
+    ws = np.array([ p[0] for p in hist["params"]])
+    rng = max(abs(ws[:,0].min()),abs(ws[:,0].max()))
+    wr = np.linspace(-rng+0.27,rng+0.27,20)
+    cst = [compute_cost(X,y,np.array([wr[i],-32, -67, -1.46]), 221) for i in range(len(wr))]
+
+    fig,ax = plt.subplots(1,2,figsize=(12,3))
+    ax[0].plot(hist["iter"], (hist["cost"]));  ax[0].set_title("Cost vs Iteration")
+    ax[0].set_xlabel("iteration"); ax[0].set_ylabel("Cost")
+    ax[1].plot(wr, cst); ax[1].set_title("Cost vs w[0]")
+    ax[1].set_xlabel("w[0]"); ax[1].set_ylabel("Cost")
+    ax[1].plot(ws[:,0],hist["cost"])
+    plt.show()
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
+    """
+    Args:
+      X (ndarray (m,n))   : Data, m examples with n features
+      y (ndarray (m,))    : target values
+      w_in (ndarray (n,)) : initial model parameters
+      b_in (scalar)       : initial model parameter
+      cost_function       : function to compute cost
+      gradient_function   : function to compute the gradient
+      alpha (float)       : Learning rate
+      num_iters (int)     : number of iterations to run gradient descent
+    Returns:
+      w (ndarray (n,)) : Updated values of parameters
+      b (scalar)       : Updated value of parameter
+      """
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  # avoid modifying global w within function
+    b = b_in
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = gradient_function(X, y, w, b)  ##None
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw  ##None
+        b = b - alpha * dj_db  ##None
+        # Save cost J at each iteration
+        if i < 100000:  # prevent resource exhaustion
+            J_history.append(cost_function(X, y, w, b))
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i % math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}   ")
+    return w, b, J_history  # return final w,b and J history for graphing
 
 # load data
 def load_house_data():
